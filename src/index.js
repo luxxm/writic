9ca666b18982b1a0f4bf7f802bb1543602041ace
luxxm <robloxvr21@gmail.com>
2021-04-07 22:33:19 +0200

    add logos

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <div>
+    <img src="logos/logo512.png" />
+  </div>,
   document.getElementById('root')
 );
